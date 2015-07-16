09fbc2e2e213467c82fd62ef13679b7eff80c7c2
Michael Kirylo <mkirylo@gmail.com>
2015-07-16 00:00:36 -0700

    init

---
+++ Assets/player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+	public Vector2 speed = new Vector2(50,50);
+	// Use this for initialization
+	void Start () 
+	{
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		Debug.Log(inputX); 
+		Vector3 movement = new Vector3(speed.x * inputX,speed.y * inputY, 0);
+		movement *= Time.deltaTime;
+		transform.Translate(movement);
+	}
+}
